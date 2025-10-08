fae7d8b5fa0abb70cad36356054683cb13767cfc
mrrobot2524 <alenjon2594@gmail.com>
2025-10-08 20:54:47 +0500

    fix some src/utils/string/get-review-word-with-ending.ts

--- src/utils/string/get-review-word-with-ending.ts
+++ src/utils/string/get-review-word-with-ending.ts
@@ -1,8 +1,17 @@
 export const getReviewWordWithEnding = (reviewCount: number) => {
-	switch (reviewCount) {
-		case 1 || 21 || 31:
+	const lastTwo = reviewCount % 100
+	const lastDigit = reviewCount % 10
+
+	if (lastTwo >= 11 && lastTwo <= 19) {
+		return `${reviewCount} отзывов`
+	}
+
+	switch (lastDigit) {
+		case 1:
 			return `${reviewCount} отзыв`
-		case 2 || 3 || 4 || 22 || 23 || 24 || 34:
+		case 2:
+		case 3:
+		case 4:
 			return `${reviewCount} отзыва`
 		default:
 			return `${reviewCount} отзывов`
